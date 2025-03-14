8d5867f54f5fe090285616f3ec2980ecf533d1bc
artemkv <artemkv@gmail.com>
2025-03-14 23:45:30 +0100

    path from title

--- lib/conversions.dart
+++ lib/conversions.dart
@@ -13,6 +13,36 @@ String getTitleFromPath(String path) {
   return title;
 }
 
+String generatePathFromTitle(String title, bool ensureUnique) {
+  String postfix = "";
+  if (ensureUnique) {
+    var date = DateTime.now();
+    int n = date.millisecondsSinceEpoch;
+    postfix = titlePostfixSeparator + n.toString();
+  }
+  return "${encodePathFileSystemFriendly(title)}$postfix.txt";
+}
+
+String encodePathFileSystemFriendly(String path) {
+  path = path.replaceAll("/", "(sl)");
+  path = path.replaceAll("?", "(qst)");
+  path = path.replaceAll("<", "(lt)");
+  path = path.replaceAll(">", "(gt)");
+  path = path.replaceAll("\\", "(bsl)");
+  path = path.replaceAll(":", "(col)");
+  path = path.replaceAll("*", "(star)");
+  path = path.replaceAll("|", "(pipe)");
+  path = path.replaceAll("\"", "(dqt)");
+  path = path.replaceAll("^", "(crt)");
+  path = path.replaceAll("%", "(pct)");
+
+  if (path.startsWith(".")) {
+    path = "_$path";
+  }
+
+  return path;
+}
+
 String decodePathFileSystemFriendly(String path) {
   path = path.replaceAll("(sl)", "/");
   path = path.replaceAll("(qst)", "?");
--- test/conversions_test.dart
+++ test/conversions_test.dart
@@ -2,6 +2,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:notedok/conversions.dart';
 
 void main() {
+  // Title from path
+
   test('get title from path', () {
     var title = getTitleFromPath("Hello, World.txt");
     expect(title, "Hello, World");
@@ -32,5 +34,32 @@ void main() {
   test('get title from short path', () {
     var title = getTitleFromPath("1.txt");
     expect(title, "1");
+  });
+
+  // Path from title
+
+  test('generate path from title', () {
+    var path = generatePathFromTitle("Hello, World", false);
+    expect(path, "Hello, World.txt");
+  });
+
+  test('generate unique path from title', () {
+    var path = generatePathFromTitle("Hello, World", true);
+    expect(path.startsWith("Hello, World~~"), true);
+    expect(path.endsWith(".txt"), true);
+  });
+
+  test('generate unique path from empty title', () {
+    var path = generatePathFromTitle("", true);
+    expect(path.startsWith("~~"), true);
+    expect(path.endsWith(".txt"), true);
+  });
+
+  test('generate path from empty title encode chars', () {
+    var path = generatePathFromTitle("Hello, World /?<>\\:*|\"^%", false);
+    expect(
+      path,
+      "Hello, World (sl)(qst)(lt)(gt)(bsl)(col)(star)(pipe)(dqt)(crt)(pct).txt",
+    );
   });
 }
